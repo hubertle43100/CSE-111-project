e3051b3d03040151cbad457a44a881af8bbe6a88
RevealSquid <hubertle43100@gmail.com>
2021-11-11 23:41:02 -0800

    starbucks csv file

---
+++ empty-tables.sql
@@ -0,0 +1,2 @@
+delete from drinks;
+delete from food;
---
+++ load-tpch.sql
@@ -0,0 +1,5 @@
+.mode "csv"
+.separator ","
+.headers off
+.import data/starbucks-menu-nutrition-food.csv food
+.import data/starbucks_drinkMenu_expanded.csv drink
